350c8f00c96b2b177f3d60ca559df09891c0eae8
Nicolás Snider <nicolas.snider@outlook.com>
2023-09-01 17:48:31 -0300

    update middleware

--- index.ts
+++ index.ts
@@ -3,6 +3,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use((req,res,next)=>{
+	res.setHeader('Access-Control-Allow-Origin','*');
+	res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
+	res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+	next();
+});
+
 app.get('/',(_req,res)=>{
 	res.send('Hello World');
 })
